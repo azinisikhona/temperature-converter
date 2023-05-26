db29fa3cf1625acdef7e1de27b57eb72bd959aad
azinisikhona <azizsiyalana10@gmail.com>
2023-05-26 09:36:16 +0200

    working

--- script/index.js
+++ script/index.js
@@ -1,23 +1,34 @@
 function convert(type) {
-    var input1 = document.getElementById("fahrenheit");
-    var input2 = document.getElementById("celsius");
-    var output1 = document.getElementById("answer1");
-    var output2 = document.getElementById("answer2");
+    let input1 = document.getElementById("fahrenheit");
+    let input2 = document.getElementById("celsius");
+    let output1 = document.getElementById("answer1");
+    let output2 = document.getElementById("answer2");
     if (type === "fahrenheit") {
-      var fahrenheit = parseFloat(input1.value);
+      let fahrenheit = parseFloat(input1.value);
       if (!isNaN(fahrenheit)) {
-        var celsius = (fahrenheit - 32) * (5 / 9);
+        let celsius = (fahrenheit - 32) * (5 / 9);
         output1.value = celsius.toFixed(2) + " °C";
       } else {
         output1.value = "Invalid input";
       }
     } else if (type === "celsius") {
-      var celsius = parseFloat(input2.value);
+      let celsius = parseFloat(input2.value);
       if (!isNaN(celsius)) {
-        var fahrenheit = (celsius * (9 / 5)) + 32;
+        let fahrenheit = (celsius * (9 / 5)) + 32;
         output2.value = fahrenheit.toFixed(2) + " °F";
       } else {
         output2.value = "Invalid input";b
       }
     }
   }
+
+let erase = document.getElementById("fahrenheit")
+let erase2 = document.getElementById("celsius")
+let erase3 = document.getElementById("answer1")
+let erase4 = document.getElementById("answer2")
+let clearAll = () =>{
+    erase.value = '';
+    erase2.value = '';
+    erase3.value = '';
+    erase4.value = '';
+}
